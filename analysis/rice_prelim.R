a0a6ab02c47ffb43eb0f6cad0cb5edf377e506f4
Enrico Crema <erc62@cam.ac.uk>
2023-02-23 16:30:21 +0000

    Updated preliminary analysis with three regions

--- analysis/rice_prelim.R
+++ analysis/rice_prelim.R
@@ -23,3 +23,25 @@ par(mfrow=c(2,1))
 plot(stspd.sw,type='proportion',col.fill=c('steelblue','lightgrey'),main='SW Japan')
 plot(stspd.ne,type='proportion',col.fill=c('steelblue','lightgrey'),main='NE Japan')
 
+
+ricedata$Region3  <- 'SW'
+ricedata$Region3[which(ricedata$Region %in% c('Chubu','Kanto'))] = 'C'
+ricedata$Region3[which(ricedata$Region %in% c('Tohoku'))] = 'NE'
+
+ricedata.sw3  <- subset(ricedata,Region3=='SW')
+ricedata.c3  <- subset(ricedata,Region3=='C')
+ricedata.ne3  <- subset(ricedata,Region3=='NE')
+
+caldates.sw3 <- calibrate(ricedata.sw3$C14Age,ricedata.sw3$C14Error) 
+caldates.c3 <- calibrate(ricedata.c3$C14Age,ricedata.c3$C14Error)
+caldates.ne3 <- calibrate(ricedata.ne3$C14Age,ricedata.ne3$C14Error)
+
+stspd.sw3  <- stackspd(caldates.sw3,runm = 20,timeRange=c(4000,1750),group = ricedata.sw3$group)
+stspd.ne3  <- stackspd(caldates.ne3,runm = 20,timeRange=c(4000,1750),group = ricedata.ne3$group)
+stspd.c3  <- stackspd(caldates.c3,runm = 20,timeRange=c(4000,1750),group = ricedata.c3$group)
+
+
+par(mfrow=c(3,1))
+plot(stspd.sw3,type='proportion',col.fill=c('steelblue','lightgrey'),main='SW Japan')
+plot(stspd.c3,type='proportion',col.fill=c('steelblue','lightgrey'),main='C Japan')
+plot(stspd.ne3,type='proportion',col.fill=c('steelblue','lightgrey'),main='NE Japan')
