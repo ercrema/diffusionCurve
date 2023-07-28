2f9a532fc78366988bcc14eb0662c896fe8f93c0
Enrico Crema <erc62@cam.ac.uk>
2023-07-28 08:27:19 +0100

    Updated burial pre-processing to filter dates in burial_clean.R

--- data/burial_clean.R
+++ data/burial_clean.R
@@ -6,5 +6,9 @@ library(rcarbon)
 burial  <- read.csv(here('data','raw','burialdates.csv'))|> select(SiteID,Country,CaseStudyArea,LabID,Material,CRA,Error,Longitude,Latitude,Disarticulated,Cremation) 
 burial$Cremation  <- ifelse(burial$Cremation=='Yes',1,0)
 burial$Disarticulated  <- ifelse(burial$Disarticulated=='Yes',1,0)
+cal  <- calibrate(burial$CRA,burial$Error)
+burial  <- burial[which.CalDates(cal,BP<5500 & BP>2000,p=0.99),]
+nrow(burial)
+table(burial$Cremation)
 
 save(burial,file=here('data','burialdata.RData'))
