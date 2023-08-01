87704b275aec17d1b14a8d6205c395b8b13f92e1
Enrico Crema <erc62@cam.ac.uk>
2023-08-01 15:19:39 +0100

    Updated script to changes in ppmat params list

--- analysis/post_check_jp_abot.R
+++ analysis/post_check_jp_abot.R
@@ -8,11 +8,11 @@ load(here('results','ppcheck_jp_abot.RData'))
 
 caldates.jp.abot  <- calibrate(d.jp.abot$cra,constants.jp.abot$cra_error)
 obs.bin.jp.abot  <- d.jp.abot$y
-fitted.mat  <- apply(ppmat.params.abot,1,function(x){sigmoid(4000:1700,r=x[1],m=x[2],k=x[3])})
+fitted.mat  <- apply(ppmat.params.jp.abot,1,function(x){sigmoid(4000:1700,r=x[1],m=x[2],k=x[3])})
 fitted.mean.abot  <- apply(fitted.mat,1,mean)
 fitted.90env.abot  <- apply(fitted.mat,1,quantile,prob=c(0.1,0.9))
 
 
 ppc.jp.abot  <- ppcheck(caldates.jp.abot,obs.bin.jp.abot,ppmat.jp.abot,timeRange=c(4000,1700))
 
-save(ppc.jp.abot,file=here('results','ppc_jp_abot.RData'))
+save(ppc.jp.abot,fitted.mean.abot,fitted.90env.abot,file=here('results','ppc_jp_abot.RData'))
