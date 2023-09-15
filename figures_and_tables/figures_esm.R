2bfb6bee489011e525db9adb5be902515ef8cd70
Enrico Crema <erc62@cam.ac.uk>
2023-09-15 10:12:11 +0100

    Final set of figures

---
+++ figures_and_tables/figures_esm.R
@@ -0,0 +1,246 @@
+library(here)
+library(nimbleCarbon)
+library(latex2exp)
+library(RColorBrewer)
+library(emdbook)
+library(coda)
+
+# Figure S1 ----
+load(here('sim','simdata','simdata1.RData'))
+load(here('sim','results','post_sim1.RData'))
+
+pdf(file=here('figures_and_tables','figureS1.pdf'),width=7,height=7)
+layout(matrix(c(1,2,3,4,5,10,14,15,16,6,20,11,17,18,7,20,20,12,19,8,20,20,20,13,9),nrow=5,ncol=5),width=c(0.1,1,1,1,1),height=c(1,1,1,1,0.1))
+
+label.plot <- function(label)
+{
+	par(mar=c(0,0,0,0))
+	plot(NULL,xlim=c(0,1),ylim=c(0,1),axes=FALSE)
+	text(label,x=0.5,y=0.5,las=2,adj=0.5,cex=1.5)
+}
+
+label.plot('r')
+label.plot('m')
+label.plot(TeX(r'($\mu$)'))
+label.plot(TeX(r'($\varphi$)'))
+
+label.plot('')
+label.plot('r')
+label.plot('m')
+label.plot(TeX(r'($\mu$)'))
+label.plot(TeX(r'($\varphi$)'))
+
+par(mar=c(2,2,2,2))
+postHPDplot(post.sample.core.sim1$r,xlab='',ylab='',axes=F)
+axis(1,line=0,padj=-1,tck=-0.05,cex.axis=0.8)
+abline(v=true.param.1$r,lty=2,col=2)
+postHPDplot(post.sample.core.sim1$m,xlab='',ylab='',axes=F)
+axis(1,line=0,padj=-1,tck=-0.05,cex.axis=0.8)
+abline(v=true.param.1$m,lty=2,col=2)
+postHPDplot(post.sample.core.sim1$mu_k,xlab='',ylab='',axes=F)
+axis(1,line=0,padj=-1,tck=-0.05,cex.axis=0.8)
+abline(v=true.param.1$mu_k,lty=2,col=2)
+postHPDplot(post.sample.core.sim1$phi,xlab='',ylab='',axes=F)
+axis(1,line=0,padj=-1,tck=-0.05,cex.axis=0.8)
+abline(v=true.param.1$phi,lty=2,col=2)
+
+
+post.sample.core.sim1  <- post.sample.core.sim1[,c('r','m','mu_k','phi')]
+true.param.1 <- true.param.1[c('r','m','mu_k','phi')]
+
+
+for (i in 1:4)
+{
+	for (j in 1:4)
+	{
+		if (j>i){
+			plot(post.sample.core.sim1[,i],post.sample.core.sim1[,j],pch=1,col=rgb(0.67,0.84,0.9,0.1))
+			HPDregionplot(mcmc(post.sample.core.sim1[,c(i,j)]),add=TRUE,prob=c(0.5,0.95),n=100,lty=c(1,2))
+			points(true.param.1[i],true.param.1[j],pch="+",col='red',cex=1.5)
+		}
+	}
+}
+dev.off()
+
+
+
+
+# Figure S2 ----
+load(here('sim','simdata','simdata2.RData'))
+load(here('sim','results','post_sim2.RData'))
+
+pdf(file=here('figures_and_tables','figureS2.pdf'),width=7,height=7)
+layout(matrix(c(1,2,3,4,5,10,14,15,16,6,20,11,17,18,7,20,20,12,19,8,20,20,20,13,9),nrow=5,ncol=5),width=c(0.1,1,1,1,1),height=c(1,1,1,1,0.1))
+
+label.plot <- function(label)
+{
+	par(mar=c(0,0,0,0))
+	plot(NULL,xlim=c(0,1),ylim=c(0,1),axes=FALSE)
+	text(label,x=0.5,y=0.5,las=2,adj=0.5,cex=1.5)
+}
+
+label.plot('r')
+label.plot('m')
+label.plot(TeX(r'($\mu$)'))
+label.plot(TeX(r'($\varphi$)'))
+
+label.plot('')
+label.plot('r')
+label.plot('m')
+label.plot(TeX(r'($\mu$)'))
+label.plot(TeX(r'($\varphi$)'))
+
+par(mar=c(2,2,2,2))
+postHPDplot(post.sample.core.sim2$r,xlab='',ylab='',axes=F)
+axis(1,line=0,padj=-1,tck=-0.05,cex.axis=0.8)
+abline(v=true.param.2$r,lty=2,col=2)
+postHPDplot(post.sample.core.sim2$m,xlab='',ylab='',axes=F)
+axis(1,line=0,padj=-1,tck=-0.05,cex.axis=0.8)
+abline(v=true.param.2$m,lty=2,col=2)
+postHPDplot(post.sample.core.sim2$mu_k,xlab='',ylab='',axes=F)
+axis(1,line=0,padj=-1,tck=-0.05,cex.axis=0.8)
+abline(v=true.param.2$mu_k,lty=2,col=2)
+postHPDplot(post.sample.core.sim2$phi,xlab='',ylab='',axes=F)
+axis(1,line=0,padj=-1,tck=-0.05,cex.axis=0.8)
+abline(v=true.param.2$phi,lty=2,col=2)
+
+
+post.sample.core.sim2  <- post.sample.core.sim2[,c('r','m','mu_k','phi')]
+true.param.2 <- true.param.2[c('r','m','mu_k','phi')]
+
+for (i in 1:4)
+{
+	for (j in 1:4)
+	{
+		if (j>i){
+			plot(post.sample.core.sim2[,i],post.sample.core.sim2[,j],pch=1,col=rgb(0.67,0.84,0.9,0.1))
+			HPDregionplot(mcmc(post.sample.core.sim2[,c(i,j)]),add=TRUE,prob=c(0.5,0.95),n=100,lty=c(1,2))
+			points(true.param.2[i],true.param.2[j],pch="+",col='red',cex=1.5)
+		}
+	}
+}
+dev.off()
+
+
+
+# Figure S3 ----
+load(here('results','post_jp_abot.RData'))
+source(here('src','utility.R'))
+pdf(file=here('figures_and_tables','figureS3.pdf'),width=5,height=4)
+plot.fitted(r=post.sample.core.jp.abot[,'r'],m=post.sample.core.jp.abot[,'m'],mu_k=post.sample.core.jp.abot[,'mu_k'],timeRange=c(4000,1700),calendar = 'BCAD',xlab='',ylab='')
+mtext('BCE/CE',side=1,line=2.2,cex=0.7)
+mtext('Proportion',side=2,line=2.2,cex=0.7)
+dev.off()
+
+# Figure S4 ----
+pdf(file=here('figures_and_tables','figureS4.pdf'),width=7,height=7)
+layout(matrix(c(1,2,3,4,5,10,14,15,16,6,20,11,17,18,7,20,20,12,19,8,20,20,20,13,9),nrow=5,ncol=5),width=c(0.1,1,1,1,1),height=c(1,1,1,1,0.1))
+
+label.plot <- function(label)
+{
+	par(mar=c(0,0,0,0))
+	plot(NULL,xlim=c(0,1),ylim=c(0,1),axes=FALSE)
+	text(label,x=0.5,y=0.5,las=2,adj=0.5,cex=1.5)
+}
+
+label.plot('r')
+label.plot('m')
+label.plot(TeX(r'($\mu$)'))
+label.plot(TeX(r'($\varphi$)'))
+
+label.plot('')
+label.plot('r')
+label.plot('m')
+label.plot(TeX(r'($\mu$)'))
+label.plot(TeX(r'($\varphi$)'))
+
+par(mar=c(2,2,2,2))
+postHPDplot(post.sample.core.jp.abot$r,xlab='',ylab='',axes=F)
+axis(1,line=0,padj=-1,tck=-0.05,cex.axis=0.8)
+postHPDplot(post.sample.core.jp.abot$m,xlab='',ylab='',axes=F)
+axis(1,line=0,padj=-1,tck=-0.05,cex.axis=0.8)
+postHPDplot(post.sample.core.jp.abot$mu_k,xlab='',ylab='',axes=F)
+axis(1,line=0,padj=-1,tck=-0.05,cex.axis=0.8)
+postHPDplot(post.sample.core.jp.abot$phi,xlab='',ylab='',axes=F)
+axis(1,line=0,padj=-1,tck=-0.05,cex.axis=0.8)
+
+
+post.sample.core.jp.abot  <- post.sample.core.jp.abot[,c('r','m','mu_k','phi')]
+true.param.1 <- true.param.1[c('r','m','mu_k','phi')]
+
+
+for (i in 1:4)
+{
+	for (j in 1:4)
+	{
+		if (j>i){
+			plot(post.sample.core.jp.abot[,i],post.sample.core.jp.abot[,j],pch=1,col=rgb(0.67,0.84,0.9,0.1))
+			HPDregionplot(mcmc(post.sample.core.jp.abot[,c(i,j)]),add=TRUE,prob=c(0.5,0.95),n=100,lty=c(1,2))
+		}
+	}
+}
+dev.off()
+
+
+
+
+# Figure S5 ----
+load(here('results','post_gb_abot.RData'))
+source(here('src','utility.R'))
+pdf(file=here('figures_and_tables','figureS5.pdf'),width=5,height=4)
+plot.fitted(r=post.sample.core.gb.abot[,'r'],m=post.sample.core.gb.abot[,'m'],mu_k=post.sample.core.gb.abot[,'mu_k'],timeRange=c(7000,3000),calendar = 'BCAD',xlab='',ylab='')
+mtext('BCE',side=1,line=2.2,cex=0.7)
+mtext('Proportion',side=2,line=2.2,cex=0.7)
+dev.off()
+
+# Figure S6 ----
+
+pdf(file=here('figures_and_tables','figureS6.pdf'),width=7,height=7)
+layout(matrix(c(1,2,3,4,5,10,14,15,16,6,20,11,17,18,7,20,20,12,19,8,20,20,20,13,9),nrow=5,ncol=5),width=c(0.1,1,1,1,1),height=c(1,1,1,1,0.1))
+
+label.plot <- function(label)
+{
+	par(mar=c(0,0,0,0))
+	plot(NULL,xlim=c(0,1),ylim=c(0,1),axes=FALSE)
+	text(label,x=0.5,y=0.5,las=2,adj=0.5,cex=1.5)
+}
+
+label.plot('r')
+label.plot('m')
+label.plot(TeX(r'($\mu$)'))
+label.plot(TeX(r'($\varphi$)'))
+
+label.plot('')
+label.plot('r')
+label.plot('m')
+label.plot(TeX(r'($\mu$)'))
+label.plot(TeX(r'($\varphi$)'))
+
+par(mar=c(2,2,2,2))
+postHPDplot(post.sample.core.gb.abot$r,xlab='',ylab='',axes=F)
+axis(1,line=0,padj=-1,tck=-0.05,cex.axis=0.8)
+postHPDplot(post.sample.core.gb.abot$m,xlab='',ylab='',axes=F)
+axis(1,line=0,padj=-1,tck=-0.05,cex.axis=0.8)
+postHPDplot(post.sample.core.gb.abot$mu_k,xlab='',ylab='',axes=F)
+axis(1,line=0,padj=-1,tck=-0.05,cex.axis=0.8)
+postHPDplot(post.sample.core.gb.abot$phi,xlab='',ylab='',axes=F)
+axis(1,line=0,padj=-1,tck=-0.05,cex.axis=0.8)
+
+
+post.sample.core.gb.abot  <- post.sample.core.gb.abot[,c('r','m','mu_k','phi')]
+true.param.1 <- true.param.1[c('r','m','mu_k','phi')]
+
+
+for (i in 1:4)
+{
+	for (j in 1:4)
+	{
+		if (j>i){
+			plot(post.sample.core.gb.abot[,i],post.sample.core.gb.abot[,j],pch=1,col=rgb(0.67,0.84,0.9,0.1))
+			HPDregionplot(mcmc(post.sample.core.gb.abot[,c(i,j)]),add=TRUE,prob=c(0.5,0.95),n=100,lty=c(1,2))
+		}
+	}
+}
+dev.off()
+
+
--- figures_and_tables/table_main.R
+++ figures_and_tables/table_main.R
@@ -7,32 +7,33 @@ load(here('results','post_gb_abot.RData'))
 
 hpd.r.jp <- post.sample.core.jp.abot[,'r'] |> as.mcmc() |> HPDinterval() |> as.numeric() |> round(4) |> paste0(collapse='~')
 median.r.jp <- post.sample.core.jp.abot[,'r'] |> as.mcmc() |> median() |> as.numeric() |> round(4)
-hpd.k.jp <- post.sample.core.jp.abot[,'mu_k'] |> logistic() |> as.mcmc() |> HPDinterval() |> as.numeric() |> round(3) |> paste0(collapse='~')
-median.k.jp <- post.sample.core.jp.abot[,'mu_k'] |> logistic() |> as.mcmc() |> median() |> as.numeric() |> round(3)
-hpd.sigma.jp  <- post.sample.core.jp.abot[,'sigma_k'] |> as.mcmc() |> HPDinterval() |> round(2) |> paste0(collapse='~')
-median.sigma.jp  <- post.sample.core.jp.abot[,'sigma_k'] |> as.mcmc() |> median() |> round(2)
+hpd.k.jp <- post.sample.core.jp.abot[,'mu_k'] |> as.mcmc() |> HPDinterval() |> as.numeric() |> round(3) |> paste0(collapse='~')
+median.k.jp <- post.sample.core.jp.abot[,'mu_k'] |> as.mcmc() |> median() |> as.numeric() |> round(3)
+hpd.phi.jp  <- post.sample.core.jp.abot[,'phi'] |> as.mcmc() |> HPDinterval() |> round(2) |> paste0(collapse='~')
+median.phi.jp  <- post.sample.core.jp.abot[,'phi'] |> as.mcmc() |> median() |> round(2)
 hpd.m.jp <- post.sample.core.jp.abot[,'m'] |> as.mcmc() |> HPDinterval() |> as.numeric() |> round() |> BPtoBCAD() |> sort()
 hpd.m.jp <- ifelse(hpd.m.jp<0,paste0('BC',abs(hpd.m.jp)),paste0('AD',hpd.m.jp)) |> paste0(collapse='~')
 median.m.jp <- post.sample.core.jp.abot[,'m'] |> as.mcmc() |> median() |> as.numeric() |> round() |> BPtoBCAD()
 median.m.jp  <- ifelse(median.m.jp<0,paste0('BC',abs(median.m.jp)),paste0('AD',median.m.jp))
-rhats.jp  <- rhats.jp.abot[[1]][c('r','m','mu_k','sigma_k'),1]
+rhats.jp  <- rhats.jp.abot[[1]][c('r','m','mu_k','phi'),1]
 
 hpd.r.gb <- post.sample.core.gb.abot[,'r'] |> as.mcmc() |> HPDinterval() |> as.numeric() |> round(4) |> paste0(collapse='~')
 median.r.gb <- post.sample.core.gb.abot[,'r'] |> as.mcmc() |> median() |> as.numeric() |> round(4)
-hpd.k.gb <- post.sample.core.gb.abot[,'mu_k'] |> logistic() |> as.mcmc() |> HPDinterval() |> as.numeric() |> round(3) |> paste0(collapse='~')
-median.k.gb <- post.sample.core.gb.abot[,'mu_k'] |> logistic() |> as.mcmc() |> median() |> as.numeric() |> round(3)
-hpd.sigma.gb  <- post.sample.core.gb.abot[,'sigma_k'] |> as.mcmc() |> HPDinterval() |> round(2) |> paste0(collapse='~')
-median.sigma.gb  <- post.sample.core.gb.abot[,'sigma_k'] |> as.mcmc() |> median() |> round(2)
+hpd.k.gb <- post.sample.core.gb.abot[,'mu_k'] |> as.mcmc() |> HPDinterval() |> as.numeric() |> round(3) |> paste0(collapse='~')
+median.k.gb <- post.sample.core.gb.abot[,'mu_k'] |> as.mcmc() |> median() |> as.numeric() |> round(3)
+hpd.phi.gb  <- post.sample.core.gb.abot[,'phi'] |> as.mcmc() |> HPDinterval() |> round(2) |> paste0(collapse='~')
+median.phi.gb  <- post.sample.core.gb.abot[,'phi'] |> as.mcmc() |> median() |> round(2)
 hpd.m.gb <- post.sample.core.gb.abot[,'m'] |> as.mcmc() |> HPDinterval() |> as.numeric() |> round() |> BPtoBCAD() |> sort()
 hpd.m.gb <- ifelse(hpd.m.gb<0,paste0('BC',abs(hpd.m.gb)),paste0('AD',hpd.m.gb)) |> paste0(collapse='~')
 median.m.gb <- post.sample.core.gb.abot[,'m'] |> as.mcmc() |> median() |> as.numeric() |> round() |> BPtoBCAD()
 median.m.gb  <- ifelse(median.m.gb<0,paste0('BC',abs(median.m.gb)),paste0('AD',median.m.gb))
-rhats.gb  <- rhats.gb.abot[[1]][c('r','m','mu_k','sigma_k'),1]
+rhats.gb  <- rhats.gb.abot[[1]][c('r','m','mu_k','phi'),1]
 
 
-table1  <- data.frame(Region=c(rep('Japan',4),rep('Britain',4)),Parameters=rep(c('r','m','logit(mu_k))','sigma_k'),2))
-table1$Median <- c(median.r.jp,median.m.jp,median.k.jp,median.sigma.jp,median.r.gb,median.m.gb,median.k.gb,median.sigma.gb)
-table1$HPD <- c(hpd.r.jp,hpd.m.jp,hpd.k.jp,hpd.sigma.jp,hpd.r.gb,hpd.m.gb,hpd.k.gb,hpd.sigma.gb)
+
+table1  <- data.frame(Region=c(rep('Japan',4),rep('Britain',4)),Parameters=rep(c('r','m','mu','phi'),2))
+table1$Median <- c(median.r.jp,median.m.jp,median.k.jp,median.phi.jp,median.r.gb,median.m.gb,median.k.gb,median.phi.gb)
+table1$HPD <- c(hpd.r.jp,hpd.m.jp,hpd.k.jp,hpd.phi.jp,hpd.r.gb,hpd.m.gb,hpd.k.gb,hpd.phi.gb)
 table1$Rhats  <- c(rhats.jp,rhats.gb) |> round(4)
 
 
